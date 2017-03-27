818b24a7b64f69dadde61752ebcc0eac2f277238
Carlos Tiago <ontiago@gmail.com>
2017-03-27 07:07:56 -0300

    Erro em Navigation.PushAsync

---
+++ Modulo1/Modulo1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Modulo1.Pages;
+using Xamarin.Forms;
+
+namespace Modulo1
+{
+	public class App : Application
+	{
+		public App()
+		{
+			// The root page of your application
+
+
+			MainPage = new MenuPage();
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
